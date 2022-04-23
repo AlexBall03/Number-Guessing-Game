ea7ad58737e306e317dd853690414b966a658d38
AlexBall03 <alexanderdball03@gmail.com>
2022-04-23 14:25:34 -0700

    Add YouTube link to README.md and wrote some comments in Main.c

--- Main.c
+++ Main.c
@@ -1,5 +1,34 @@
-// HOW THIS WORKS:
-// Think of a number from 1 to 100, you can guess you number within 7 turns.
+/*
+    Think of a number from 1 to 100, you can guess you number within 7 turns
+
+    Is your number 50?
+    No, guess a higher number
+
+    Is your number 75?
+    No, guess a higher number
+
+    Is your number 88?
+    No, guess a lower number
+
+    Is your number 81?
+    Yes, you guess my number within 7 turns! (You guessed it in 4 turns)
+    --------------------------------------------------------------------------
+    HOW THIS WORKS:
+    Think of a number from 1 to 100, you can guess you number within 7 turns.
+
+    Is your number roundUp(100 + 1 - 1) / 2 == 50? Range 1 to 100
+    No, guess a higher number (newRange = 51 to 100)(50 possible guesses)
+
+    Is your number roundUp(100 + 51 - 1) / 2 == 75? Range 51 to 100
+    No, guess a higher number  (newRange = 76 to 100)(25 possible guesses)
+
+    Is your number roundUp(100 + 76 - 1) / 2 == 87.5 == 88? Range 76 to 100
+    No, guess a lower number (newRange = 76 to 87) (12 possible guesses)
+
+    Is your number roundUp(87 + 76 -1) / 2 == 81?
+    Yes, you guess my number within 7 turns! (You guessed it in 4 turns)
+*/
+
 #include <stdio.h>
 
 int main() {
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## This is a number guessing game. The goal is to think of a number between 1 and 100 before your limit of 7 turns is up.
 
-#### For now, this will be strictly based on a YouTube video. Over time, I will make my own changes as I learn.  Later in the future, I'll make it so that you can choose how many turns you have and the number range. And hopefully even more advanced! 
+#### For now, this will be strictly based on a YouTube video. Over time, I will make my own changes as I learn.  Later in the future, I'll make it so that you can choose how many turns you have and the number range. And hopefully even more advanced!                                     https://www.youtube.com/watch?v=0wHXNMRwBMg&list=PLBhJnyA0V0uKvZP6j1JMPjUUX9PwPvKhR&index=6
 
 ## Thank you! 
